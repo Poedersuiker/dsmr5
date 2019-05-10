e8d0fb5ffcde95f755f6e71d796c39b9167d06af
Peter Hendriks <peter@the-underground.nl>
2019-05-10 17:45:33 +0200

    Try-out

--- dsmr5/__init__.py
+++ dsmr5/__init__.py
@@ -1,40 +1,48 @@
 import serial
 import logging
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 class DSMR:
     def __init__(self):
-        self.version = 'v4.0'
+        self.version = 'v0.1'
+        self.logger = logging.getLogger('EnergyMeter')
+        self.logger.setLevel(logging.DEBUG)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+        self.logger.info("DSMR v5.0.2 interpreter started")
 
     def decode_line(self, line):
         line = line.decode('utf-8').strip()
-        logging.debug(line)
+        self.logger.debug(line)
         if len(line) < 5:
-            logging.warn('No data in line')
+            self.logger.warning('No data in line')
         elif line[0] == '/':
-            logging.info('Start of Telegram')
+            self.logger.info('Start of Telegram')
         elif line[0] == '!':
-            logging.info('End of Telegram')
+            self.logger.info('End of Telegram')
         else:
             self.interpret_data(line)
 
     def interpret_data(self, line):
-        logging.debug(line)
+        self.logger.debug(line)
         OBISref, data = line.split('(', 1)
-        logging.info(OBISref)
+        self.logger.info(OBISref)
 
         if OBISref == '1-0:1.7.0':
             self.actual_electricity_power_delivered(data)
 
     def actual_electricity_power_delivered(self, data):
-        logging.info(data[:-1])
+        self.logger.info(data[:-1])
 
 
-logging.info('Start reading energymeter')
-ser = serial.Serial('/dev/ttyUSB0', 115200, parity=serial.PARITY_NONE)
-running = 1
-dsmr = DSMR()
-while running:
-    line = ser.readline()
-    print(line)
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 115200, parity=serial.PARITY_NONE)
+    running = 1
+    dsmr = DSMR()
+    while running:
+        line = ser.readline()
+        print(line)
