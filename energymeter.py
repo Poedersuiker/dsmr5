bce88eacdb881119e52bc24d9ff5abcc4b4a4dca
Peter Hendriks <peter@the-underground.nl>
2019-05-10 17:49:37 +0200

    Try-out

--- energymeter.py
+++ energymeter.py
@@ -3,7 +3,7 @@ from dsmr5 import DSMR
 
 ser = serial.Serial('/dev/ttyUSB0', 115200, parity=serial.PARITY_NONE)
 running = 1
-dsmr = DSMR()
+dsmr = DSMR(debug=True)
 while running:
     line = ser.readline()
     dsmr.decode_line(line)
