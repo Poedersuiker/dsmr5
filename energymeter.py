d495267e6bf1cb80d999b4c8b7f601a1c5db7971
Peter Hendriks <peter@the-underground.nl>
2019-05-10 17:37:05 +0200

    Try-out

---
+++ energymeter.py
@@ -0,0 +1,13 @@
+import serial
+import logging
+from dsmr5 import DSMR
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+logging.info('Start reading energymeter')
+ser = serial.Serial('/dev/ttyUSB0', 115200, parity=serial.PARITY_NONE)
+running = 1
+dsmr = DSMR()
+while running:
+    line = ser.readline()
+    print(line)
